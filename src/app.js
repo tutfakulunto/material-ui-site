f94379adb60614bcd754d308fd6b9b2b87a3e731
Scott Johnson <c.scott.johnson@gmail.com>
2017-11-24 23:22:37 -0600

    Got webpack up and running

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <h1>hello world</h1>;
+    }
+}
+
+module.exports = App;
---
+++ webpack.dev.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const config = require('dotenv').config();
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+module.exports = {
+    devServer: {
+        host: '0.0.0.0',
+        port: 3001,
+        publicPath: '/',
+        stats: 'errors-only',
+        historyApiFallback: true,
+        contentBase: 'build'
+    },
+    entry: [
+        path.join(__dirname, 'src/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filetype: 'pug',
+            template: './src/index.pug',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPugPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: true,
+                        cacheDirectory: true
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+};
