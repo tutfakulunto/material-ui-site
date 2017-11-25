1dca288c09f9275a8a81f341d7d9d5332a4c51ca
Scott Johnson <c.scott.johnson@gmail.com>
2017-11-25 00:20:04 -0600

    Integrated material-ui into project

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'material-ui/Button';
 
 class App extends React.Component {
     render() {
-        return <h1>hello world</h1>;
+        return (
+            <Button raised color="primary">
+            Hello World
+            </Button>
+        );
     }
 }
 
