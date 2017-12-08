edadceeac38088fca44a05ac9ac19b788a9661ec
Scott Johnson <c.scott.johnson@gmail.com>
2017-12-08 17:28:24 -0600

    Added top nav bar and dialog button

--- src/app.js
+++ src/app.js
@@ -6,95 +6,43 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
-import Tooltip from 'material-ui/Tooltip';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
-import Dialog, {
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from 'material-ui/Dialog';
+import DialogsPage from './dialogsPage';
+import * as appStyles from './styles.scss';
 
 const styles = theme => ({
-  root: {
-    marginTop: theme.spacing.unit * 3,
-    width: '100%',
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+    root: {
+        width: '100%',
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
 });
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
-            Title
-          </Typography>
-          <Button color="contrast">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.root}>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <IconButton className={styles.menuButton} color="contrast" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography type="title" color="inherit" className={styles.flex}>
+                            Title
+                        </Typography>
+                        <Button color="contrast">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <DialogsPage></DialogsPage>
+            </div>
+        );
+    }
 }
 
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(ButtonAppBar);
-
-class AlertDialog extends React.Component {
-  state = {
-    open: false,
-  };
-
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleRequestClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    return (
-      <div>
-        <Button onClick={this.handleClickOpen}>Open alert dialog</Button>
-        <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
-          <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Let Google help apps determine location. This means sending anonymous location data to
-              Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleRequestClose} color="primary">
-              Disagree
-            </Button>
-            <Button onClick={this.handleRequestClose} color="primary" autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
-  }
-}
-
-export default AlertDialog;
-
-
-module.exports = App;
+export default withStyles(styles)(App);
