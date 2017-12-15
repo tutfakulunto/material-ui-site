e8b50e3777a00dede3a66e178c3465ae1aa412a9
Scott Johnson <c.scott.johnson@gmail.com>
2017-12-15 15:50:24 -0600

    Added a drawer component

--- src/app.js
+++ src/app.js
@@ -2,19 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer'
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import List from 'material-ui/List'
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
+import Hidden from 'material-ui/Hidden'
+import Divider from 'material-ui/Divider'
 import MenuIcon from 'material-ui-icons/Menu';
 import DialogsPage from './dialogsPage';
+import { mailFolderListItems, otherMailFolderListItems } from './tileData';
 import * as appStyles from './styles.scss';
+
+const drawerWidth = 240;
 
 const styles = theme => ({
     root: {
         width: '100%',
+        height: 430,
+        marginTop: theme.spacing.unit * 3,
+        zIndex: 1,
+        overflow: 'hidden',
     },
+    appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    },
+    appBar: {
+        position: 'absolute',
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('md')]: {
+          width: `calc(100% - ${drawerWidth}px)`,
+        },
+      },
+      navIconHide: {
+        [theme.breakpoints.up('md')]: {
+          display: 'none',
+        },
+      },
+      drawerHeader: theme.mixins.toolbar,
+      drawerPaper: {
+        width: 250,
+        [theme.breakpoints.up('md')]: {
+          width: drawerWidth,
+          position: 'relative',
+          height: '100%',
+        },
+      },
+      content: {
+        backgroundColor: theme.palette.background.default,
+        width: '100%',
+        padding: theme.spacing.unit * 3,
+        height: 'calc(100% - 56px)',
+        marginTop: 56,
+        [theme.breakpoints.up('sm')]: {
+          height: 'calc(100% - 64px)',
+          marginTop: 64,
+        },
+      },
     flex: {
         flex: 1,
     },
@@ -24,25 +73,86 @@ const styles = theme => ({
     },
 });
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className={styles.root}>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <IconButton className={styles.menuButton} color="contrast" aria-label="Menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography type="title" color="inherit" className={styles.flex}>
-                            Title
-                        </Typography>
-                        <Button color="contrast">Login</Button>
-                    </Toolbar>
-                </AppBar>
-                <DialogsPage></DialogsPage>
-            </div>
-        );
-    }
+class ResponsiveDrawer extends React.Component {
+    state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <div>
+        <div className={classes.drawerHeader} />
+        <Divider />
+        <List>{mailFolderListItems}</List>
+        <Divider />
+        <List>{otherMailFolderListItems}</List>
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="contrast"
+                aria-label="open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography type="title" color="inherit" noWrap>
+                Account
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Hidden mdUp>
+            <Drawer
+              type="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              onRequestClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden mdDown implementation="css">
+            <Drawer
+              type="permanent"
+              open
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <main className={classes.content}>
+            <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles)(App);
+ResponsiveDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
