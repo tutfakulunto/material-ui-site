ad62528c706ebedc13f144fa974540858995709e
Scott Johnson <c.scott.johnson@gmail.com>
2018-01-15 14:34:14 -0600

    added table to languages page

--- src/components/appLayout.js
+++ src/components/appLayout.js
@@ -122,7 +122,15 @@ const styles = (theme) => ({
             color: '#DC003E',
             textDecoration: 'underline'
         }
-    }
+    },
+    root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
 });
 
 class AppLayout extends React.Component {
--- src/modules/languages/index.js
+++ src/modules/languages/index.js
@@ -4,15 +4,53 @@ import React from 'react';
 import AppLayout from '../../components/appLayout';
 import LanguageIcon from 'material-ui-icons/Language';
 import shortid from 'shortid';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
 
 class LanguagesPage extends React.Component {
     render() {
-        return (
-            <AppLayout title={[<LanguageIcon className="page-icon" key={shortid.generate()} />, 'Languages']}>
-                <h1>Languages Page</h1>
-            </AppLayout>
-        );
+        
+        function BasicTable(props) {
+          const { classes } = props;
+
+            return (
+                <AppLayout title={[<LanguagesIcon className="page-icon" key={shortid.generate()} />, 'Languages']}>
+                    <Paper className={classes.root}>
+                      <Table className={classes.table}>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell numeric>Abbreviation</TableCell>
+                            <TableCell numeric>Family</TableCell>
+                            <TableCell numeric>Description</TableCell>
+                            <TableCell numeric>Created At</TableCell>
+                            <TableCell numeric>Updated At</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {data.map(n => {
+                            return (
+                              <TableRow key={n.id}>
+                                <TableCell>{n.name}</TableCell>
+                                <TableCell numeric>{n.abbreviation}</TableCell>
+                                <TableCell numeric>{n.family}</TableCell>
+                                <TableCell numeric>{n.desription}</TableCell>
+                                <TableCell numeric>{n.createdAt}</TableCell>
+                                <TableCell numeric>n.updatedAt</TableCell>
+                              </TableRow>
+                            );
+                          })}
+                        </TableBody>
+                      </Table>
+                    </Paper>
+                </AppLayout>
+            );
+        }
     }
 }
 
-export default LanguagesPage;
+BasicTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LanguagesPage);
