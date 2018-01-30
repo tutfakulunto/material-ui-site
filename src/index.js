5968e6c97d07b668878b0b48773e3dbeba68c379
Scott Johnson <c.scott.johnson@gmail.com>
2018-01-30 11:13:30 -0600

    added Provider, createStore, and applyMiddleware to index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './app';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
